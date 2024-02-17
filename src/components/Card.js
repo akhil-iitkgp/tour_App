64b3e33296851c678adcbb2ec105f4e42e787a77
akhil-iitkgp <akhilaashiyan17@gmail.com>
2024-02-17 19:12:38 +0530

    Completed

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,23 +7,24 @@ function Card({id,image,info,price,name,removeTour}){
         setReadmore(!readmore);
     }
     return(
-        <div>
-            <img src={image} alt="swdd"></img>
+        <div className="card">
+            <img src={image} alt="image_" className="image"  ></img>
 
 
-            <div>
-                <div>
+            <div className="tour-info">
+                <div className="tour-details">
                     <h4 className="tour-price">₹ {price}</h4>
                     <h4 className="tour-name">{name}</h4>
                     
                 </div>
 
-                <div>{description}
-                <span onClick={readmoreHandler}>{readmore ? `Show Less` : `Show More`}</span>
+                <div className="description">{description}
+                <span className="read-more"
+                    onClick={readmoreHandler}>{readmore ? `Show Less` : `Show More`}</span>
                 </div>
             </div>
 
-            <button>Not Intrested</button>
+            <button className="not-intrested" onClick={()=>removeTour(id)}>Not Intrested</button>
 
 
 
--- src/components/Tours.js
+++ src/components/Tours.js
@@ -3,22 +3,65 @@ import data from "./data";
 import Card from "./Card";
 function Tours() {
   let [tours, setTours] = useState(data);
+
+  const original_length= data.length;
+  
+
+
   function removeTour(id) {
     const newTours = tours.filter((tour) => tour.id !== id);
     setTours(newTours);
   }
-  return (
-    <div>
+
+  if(tours.length===0){
+    return(
+        <div className="refresh">
+            <h2>NO TOUR LEFT</h2>
+            <button className="btn-white " onClick={()=>setTours(data)}>Refresh</button>
+        </div>
+
+    );
+  }
+
+  if(tours.length!==original_length){
+    return(
+      <div className="container">
       <div>
-        <h2>Plan With Love</h2>
+        <h2 className="title">ExploreMore Travel Company</h2>
       </div>
 
-      <div>
+      <div className="cards">
         {tours.map((tour) => {
           return <Card key={tour.id} {...tour} removeTour={removeTour}></Card>;
         })}
       </div>
+
+      
+        <div className="ref">
+          <button className="not-int" onClick={()=>setTours(data)}>Refresh for all tours</button>
+        </div>
+      
+    </div>
+  );
+  }
+
+
+  return (
+    <div className="container">
+      <div>
+        <h2 className="title">ExploreMore Travel Company</h2>
+      </div>
+
+      <div className="cards">
+        {tours.map((tour) => {
+          return <Card key={tour.id} {...tour} removeTour={removeTour}></Card>;
+        })}
+      </div>
+
+      
     </div>
   );
 }
+
+
 export default Tours;
